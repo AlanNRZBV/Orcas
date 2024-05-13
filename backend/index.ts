e4ced9b4d893c7faeccd1879321027c151c2930d
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-05-13 19:48:35 +0600

    feat: Studio update. Add teams field

--- backend/index.ts
+++ backend/index.ts
@@ -5,6 +5,7 @@ import mongoose from 'mongoose';
 import config from './config';
 import usersRouter from './routers/users';
 import projectsRouter from './routers/projects';
+import studioRouter from './routers/studio';
 
 const app = express();
 const port = 8000;
@@ -15,6 +16,7 @@ app.use(cors());
 
 app.use('/users', usersRouter);
 app.use('/projects', projectsRouter);
+app.use('/studio', studioRouter);
 
 const run = async () => {
   await mongoose.connect(config.mongoose.db);
--- backend/models/Studio.ts
+++ backend/models/Studio.ts
@@ -50,6 +50,34 @@ const StudioSchema = new mongoose.Schema({
       },
     },
   ],
+  teams: [
+    {
+      name: {
+        type: String,
+        required: true,
+      },
+      members: [
+        {
+          userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+            validate: {
+              validator: async (value: Types.ObjectId) => {
+                const user = await User.findById(value);
+                return Boolean(user);
+              },
+              message: 'VALIDATOR ERROR: User does not exist!',
+            },
+          },
+          teamRole: {
+            type: String,
+            required: true,
+          },
+        },
+      ],
+    },
+  ],
 });
 
 const Studio = model('Studio', StudioSchema);
