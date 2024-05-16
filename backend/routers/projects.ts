87dad7ac8e71c117e68f332b6a32db9f7b086ee6
Alan Noruzbaev <alan.noruzbaev@gmail.com>
2024-05-17 01:07:28 +0600

    feat: implement assign team to project functionality

--- backend/routers/projects.ts
+++ backend/routers/projects.ts
@@ -4,6 +4,7 @@ import Project from '../models/Project';
 import { ProjectData } from '../types';
 import mongoose from 'mongoose';
 import Studio from '../models/Studio';
+import Team from '../models/Team';
 
 const projectsRouter = Router();
 
@@ -39,7 +40,6 @@ projectsRouter.get('/', auth, async (req: RequestWithUser, res, next) => {
           },
         },
       });
-
       const isEmpty = projects.length < 1;
       if (isEmpty) {
         return res.status(404).send({ message: 'Нет проектов', projects: [] });
@@ -53,15 +53,18 @@ projectsRouter.get('/', auth, async (req: RequestWithUser, res, next) => {
 
 projectsRouter.post('/add', auth, async (req: RequestWithUser, res, next) => {
   try {
-    const user = req.user?._id;
+    const userId = req.user?._id;
     const id = req.body.studioId;
     const isStudioExists = await Studio.findById(id);
-
+    // const isManager = await Studio.findOne({
+    //   'staff.userId': userId,
+    //   'staff.spec.name': 'менеджер',
+    // }); //ToDo add roles
     if (!isStudioExists) {
       return res.status(404).send({ error: 'Студия не найдена', studio: {} });
     }
 
-    if (isStudioExists.owner.toString() !== user?.toString()) {
+    if (isStudioExists.owner.toString() !== userId?.toString()) {
       return res.status(422).send({ error: 'Нет доступа', studio: {} });
     }
 
@@ -75,12 +78,12 @@ projectsRouter.post('/add', auth, async (req: RequestWithUser, res, next) => {
     const project = new Project(projectData);
     const savedProject = await project.save();
 
-    isStudioExists.projects.push(savedProject._id);
+    isStudioExists.projects.push({ projectId: savedProject._id });
     await isStudioExists.save();
 
     return res.send({ message: 'Проект успешно создан', project });
   } catch (e) {
-    if (e instanceof mongoose.Error.ValidationError) {
+    if (e instanceof mongoose.Error.ValidationError || e instanceof mongoose.Error.CastError) {
       return res.status(422).send({ message: 'Ошибка проверки данных', errorMsg: e });
     }
     next(e);
@@ -107,6 +110,31 @@ projectsRouter.patch('/update/:id', auth, async (req, res, next) => {
 
     return res.send({ message: 'Данные обновлены', project: updatedProject });
   } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError || e instanceof mongoose.Error.CastError) {
+      return res.status(422).send({ message: 'Ошибка проверки данных', errorMsg: e });
+    }
+    next(e);
+  }
+});
+
+projectsRouter.patch('/assign', auth, async (req, res, next) => {
+  try {
+    const teamId = req.query.team;
+    const projectId = req.query.project;
+    const team = await Team.findById(teamId);
+    const project = await Project.findById(projectId);
+
+    if (!team || !project) {
+      return res.status(404).send({ error: 'Данные не были найдены', project: {} });
+    }
+
+    project.team.push({ teamId: team._id });
+    await project.save();
+    return res.send({ message: `Команда успешно добавлена в проект ${project.name}`, project: project });
+  } catch (e) {
+    if (e instanceof mongoose.Error.ValidationError || e instanceof mongoose.Error.CastError) {
+      return res.status(422).send({ message: 'Ошибка проверки данных', errorMsg: e });
+    }
     next(e);
   }
 });
